f6f8af3f5cf0c6c32e886500b641441899ca4841
misaf <misaf.1990@gmail.com>
2026-09-17 20:26:11 +0000

    docs: document the four undocumented models and correct two stale rules

--- src/Models/Document.php
+++ src/Models/Document.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 use Misaf\VendraDocument\Database\Factories\DocumentFactory;
 use Misaf\VendraSupport\Contracts\ShouldLogActivity;
 use Misaf\VendraSupport\Tenancy\BelongsToTenant;
@@ -18,6 +19,22 @@ use Misaf\VendraUserProfile\Models\UserProfile;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
+/**
+ * @property int $id
+ * @property int $tenant_id
+ * @property int $user_profile_id
+ * @property string $type
+ * @property string|null $issuing_country_code
+ * @property string|null $number
+ * @property Carbon|null $issued_at
+ * @property Carbon|null $expires_at
+ * @property Carbon|null $verified_at
+ * @property array<string, mixed>|null $metadata
+ * @property string|null $notes
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon|null $deleted_at
+ */
 #[Fillable([
     'user_profile_id',
     'type',
